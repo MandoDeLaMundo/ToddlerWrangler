b3a37c3dea7894bd60c6ee9f4910eafcd2717e46
cmiranda1101 <christianmiranda2@yahoo.com>
2025-03-04 17:45:41 -0500

    Play Pen Logic Working
    Got some basic logic for the play pen to work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     float mSpeed = 10.0f;
     float mZRot = 0.0f;
     float mRotSpeed = 400.0f;
+    int mBabiesHeld = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,5 +43,15 @@ public class PlayerController : MonoBehaviour
             gameObject.transform.rotation = Quaternion.Euler(0, 0, mZRot);
         }
 
+    }
+
+    void DropOffBabies()
+    {
+        Debug.Log("Babies dropped off");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        DropOffBabies();
     }
 }
