5dc535054fdcbda0228cddd1f35d3b5ed6ed8f48
DcentGaming <dabarker2000@yahoo.com>
2025-03-04 17:22:15 -0500

    Baby Collision

--- Assets/Scripts/Detect Collision Baby.cs
+++ Assets/Scripts/Detect Collision Baby.cs
@@ -18,6 +18,9 @@ public class DetectColliion : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.gameObject.tag == "Player") 
+        {
         Destroy(gameObject);
+        }
     }
 }
