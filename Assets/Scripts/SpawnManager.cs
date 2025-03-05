d411b2e20dbaff6598f96cfd32c449a754744efd
Montiel_Armando <armtiel12@gmail.com>
2025-03-04 19:33:47 -0500

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] mBabyPrefabs;
-    public int mSpawnTimer = Random.Range(2, 4);
+    public int mSpawnTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        mSpawnTimer -= deltaTime;
-        if (mSpawnTimer = 0)
+        mSpawnTimer = Random.Range(0, 50);
+        if (mSpawnTimer == 0)
         {
             int babyIndex = Random.Range(0, mBabyPrefabs.Length);
-            Instatiate(mBabyPrefabs[babyIndex], new Vector3(0, 0, 0), mBabyPrefabs[babyIndex].transform.rotation);
+            Instantiate(mBabyPrefabs[babyIndex], new Vector3(0, 0, 0), mBabyPrefabs[babyIndex].transform.rotation);
+            Instantiate(mBabyPrefabs[babyIndex], new Vector3(Random.Range(-15, 15), -6, 0), mBabyPrefabs[babyIndex].transform.rotation);
         }
-        mSpawnTimer = Random.Range(2, 4);
     }
 }
